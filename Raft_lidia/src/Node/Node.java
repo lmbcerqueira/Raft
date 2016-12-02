1a63290df9ddbb392c244ec4127b1cf9f28a5043
joaqu <joaqu@172.30.12.6>
2016-12-02 11:20:38 +0000

    Aqui vai o primeiro

--- Raft_lidia/src/Node/Node.java
+++ Raft_lidia/src/Node/Node.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package raft_lidia;
+package Node;
 
 /**
  *
  * @author joaqu
  */
-public class Raft_lidia {
+public class Node {
 
     /**
      * @param args the command line arguments
